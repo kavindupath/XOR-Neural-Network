6b124a4e769d0254685ff7643a251664ed76323b
kavindu <kavindupath@gmail.com>
2019-01-15 23:10:48 +0530

    Libraries added

--- XOR.py
+++ XOR.py
@@ -1,11 +1,11 @@
->>> import tensorflow as tf,os,sys,numpy as np,matplotlib.pyplot as plt,sounddevice as sd,soundfile as sf,keras,importlib
+import tensorflow as tf,os,sys,numpy as np,matplotlib.pyplot as plt,sounddevice as sd,soundfile as sf,keras,importlib
 Using TensorFlow backend.
->>> from keras.layers import Input,Flatten,Dense
->>> from keras.models import Sequential
->>> x=np.array([[0,0],[0,1],[1,0],[1,1]])
->>> y=np.array([0,1,1,0])
->>> mod=Sequential()
->>> mod.add(Dense(16,input_dim=2,activation="relu"))
->>> mod.add(Dense(1,activation="sigmoid"))
->>> mod.compile(loss="mean_squared_error",optimizer="adam",metrics=["binary_accuracy"])
->>> mod.fit(x,y,epochs=500,verbose=2)
+from keras.layers import Input,Flatten,Dense
+from keras.models import Sequential
+x=np.array([[0,0],[0,1],[1,0],[1,1]])
+y=np.array([0,1,1,0])
+mod=Sequential()
+mod.add(Dense(16,input_dim=2,activation="relu"))
+mod.add(Dense(1,activation="sigmoid"))
+mod.compile(loss="mean_squared_error",optimizer="adam",metrics=["binary_accuracy"])
+mod.fit(x,y,epochs=500,verbose=2)
